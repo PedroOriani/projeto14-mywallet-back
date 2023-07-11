3a0a82c65f4bad0ac7fe6ec33d471cb60344425d
Pedro Sebinelli de Godoy Oriani <TRPEDORI@traumec.com.br>
2023-07-11 16:50:53 -0300

    corrigindo get transactions

--- src/schemas/transaction.schema.js
+++ src/schemas/transaction.schema.js
@@ -4,5 +4,6 @@ export const schemaTransactions = Joi.object({
     description: Joi.string().required(),
     value: Joi.number().positive().precision(2).required(),
     day: Joi.required(),
-    type: Joi.required()
+    type: Joi.required(),
+    id: Joi.required()
 })
