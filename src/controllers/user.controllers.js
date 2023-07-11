3d25c6bbd73b9b8aca3316ee653f54be17c9827d
Pedro Oriani <pedro.oriani@gmail.com>
2023-07-10 22:00:11 -0300

    config deploy certo

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -46,7 +46,7 @@ export async function getUser(req, res){
         const user = await db.collection('users').findOne({ _id: session.idUser });
 
         delete user.password;
-        res.send({_id: user.id, name: user.name, email: user.email, day:day})
+        res.send(user)
     }catch (err){
         res.status(500).send(err.message);
     }
