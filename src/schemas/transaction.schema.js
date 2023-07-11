815c654b4dbb27c4aa92a27a78223c8e10a88833
Pedro Oriani <pedro.oriani@gmail.com>
2023-07-11 17:31:33 -0300

    atualizado

--- src/schemas/transaction.schema.js
+++ src/schemas/transaction.schema.js
@@ -4,6 +4,5 @@ export const schemaTransactions = Joi.object({
     description: Joi.string().required(),
     value: Joi.number().positive().precision(2).required(),
     day: Joi.required(),
-    type: Joi.required(),
-    id: Joi.required()
+    type: Joi.required()
 })
